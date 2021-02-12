36c593f8de48749fcb288e2891506a8d5a49ef5c
Deracine-19 <93745211+Deracine-19@users.noreply.github.com>
2021-02-11 21:40:58 -0600

    Pruebaaa
    Test

--- app/src/main/java/com/example/pdm_2021_i_p1_project1/MainActivity.kt
+++ app/src/main/java/com/example/pdm_2021_i_p1_project1/MainActivity.kt
@@ -1,11 +1,26 @@
 package com.example.pdm_2021_i_p1_project1
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_defeat.*
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        btnPlay.setOnClickListener{(showPlay())}
+        btnCreateWord.setOnClickListener{(showCreateWord())}
+    }
+
+    private fun showPlay() {
+        val intent = Intent(this, PlayActivity::class.java)
+        startActivity(intent)
+    }
+
+    private fun showCreateWord() {
+        val intent = Intent(this, CreateWordActivity::class.java)
+        startActivity(intent)
     }
 }
