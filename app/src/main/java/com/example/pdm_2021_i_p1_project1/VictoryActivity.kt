320a1c023e94d5c983def6e33c91fa1b074dc3c6
AMSC-Cortes <amoisessc@gmail.com>
2021-02-11 20:47:11 -0600

    Is done

--- app/src/main/java/com/example/pdm_2021_i_p1_project1/VictoryActivity.kt
+++ app/src/main/java/com/example/pdm_2021_i_p1_project1/VictoryActivity.kt
@@ -2,10 +2,12 @@ package com.example.pdm_2021_i_p1_project1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_victory.*
 
 class VictoryActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_victory)
+        btnBackToMenuFromVictory.setOnClickListener{(onBackPressed())}
     }
 }
