4b2680468142b07c4b5fbb2388dfa900186883eb
Deracine-19 <93745211+Deracine-19@users.noreply.github.com>
2021-02-11 19:52:01 -0600

    Creacion de Activities
    × Victoria
    × Derrota

---
+++ app/src/main/java/com/example/pdm_2021_i_p1_project1/DefeatActivity.kt
@@ -0,0 +1,11 @@
+package com.example.pdm_2021_i_p1_project1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DefeatActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_defeat)
+    }
+}
