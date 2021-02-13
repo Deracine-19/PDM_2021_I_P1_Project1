7f0f310398e0dda048ada6b4621b3f59de22134e
AMSC-Cortes <amoisessc@gmail.com>
2021-02-13 03:07:03 -0600

    Se añadio como hacer gif Views,buttons etc. AHi inserte uno no se que te parezca y puse un boton para guardar la palabra con su pista en un arreglo primera columna palabra segunda columna pista, no se bien que quedaria mejor para escrbirla asi que coloque las dos opciones varios textbox y uno grande aunque podemos colocar ese TYPE A WORD como Txv y abajo ponerte un EditText con el Write como hint.

--- app/src/main/java/com/example/pdm_2021_i_p1_project1/CreateWordActivity.kt
+++ app/src/main/java/com/example/pdm_2021_i_p1_project1/CreateWordActivity.kt
@@ -2,10 +2,55 @@ package com.example.pdm_2021_i_p1_project1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_create_word.*
 
 class CreateWordActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_word)
+
+        var word = ArrayList<String>()
+        for (elements in editTextTypeAWord.text) {
+            word.add(elements.toString())
+            var counter=0
+            counter+1
+        }
+
+        var matrizcreada=Array(5){Array<String?>(2){null} }
+        for (i in (0 until 5)){
+            matrizcreada[i][0]=editTextTypeAWord.text.toString()
+            matrizcreada[i][1]=editTextHint.text.toString()
+        }
+
+        var palabra=CharArray(editTextTypeAWord.length())
+        for (j in editTextTypeAWord.text){
+            for(i in editTextTypeAWord.text) {
+                palabra.set(0,i.toChar())
+            }
+        }
+
+        btnSave.setOnClickListener{(save())}
     }
+
+
+    private fun save() {
+        var matrizcreada = Array(5) { Array<String?>(2) { null } }
+        for (i in (0 until 5)) {
+            matrizcreada[i][0] = editTextTypeAWord.text.toString()
+            matrizcreada[i][1] = editTextHint.text.toString()
+        }
+    }
+
 }
+
+
+
+
+
+
+
+
+
+
+
